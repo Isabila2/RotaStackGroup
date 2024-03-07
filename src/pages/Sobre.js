9f5a5d5dc5272d245e07fdf32d00570023a69f3c
Isabella <125681801+Isabila2@users.noreply.github.com>
2024-03-07 20:00:03 -0300

    Update Sobre.js

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,7 +1,7 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "../styles/StyleSheet";
-import { TextInput } from "react-native-web";
+
 
 
 
@@ -14,10 +14,8 @@ export default function Sobre() {
         <TextInput style={[styles.textInput1, {borderWidth: 1}]} placeholder="Digite seu email" /> 
         <TextInput style={[styles.textInput2, {borderWidth: 2}]} placeholder="Digite sua mensagem" /> 
 
-      <Text> tela home</Text>
-      <Button title="Contato" onPress={() => navigation.navigate("Contato")} />
-      <Button title="Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      <Text> tela SOBRE</Text>
+     
     </View>
   );
 }
