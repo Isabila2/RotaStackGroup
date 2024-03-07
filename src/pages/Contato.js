9717b96e7f617accb4ff794d36511f4ac47d0586
Isabella <125681801+Isabila2@users.noreply.github.com>
2024-03-07 19:59:00 -0300

    Update Contato.js

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, TextInput } from "react-native";
+import { View, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "../styles/StyleSheet";
 
@@ -12,10 +12,6 @@ export default function Contato() {
         <TextInput style={[styles.textInput1, {borderWidth: 1}]} placeholder="Digite seu email" /> 
         <TextInput style={[styles.textInput2, {borderWidth: 2}]} placeholder="Digite sua mensagem" /> 
 
-      <Text> tela home</Text>
-      <Button title="Sobre" onPress={() => navigation.navigate("Sobre")} />
-      <Button title="Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Login" onPress={() => navigation.navigate("Login")} />
     </View>
   );
 }
