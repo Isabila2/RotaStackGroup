7144bb5b1dcc09bfab1e9497da508bf1fb99d2a5
Isabella <125681801+Isabila2@users.noreply.github.com>
2024-03-07 19:55:28 -0300

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,6 +2,7 @@ import { View, Text, Button, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "../styles/StyleSheet";
 
+// Variável da imagem
 const img = {
   uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMal0tBwFJFF0CVIe7cixk3vrardx6ESUvFA&usqp=CAU",
 };
@@ -10,12 +11,18 @@ export default function Index() {
   const navigation = useNavigation();
 
   return (
+    // Colocar imagem de fundo
+    // View com os botões
     <View style={styles.container}>
       <Text> tela home</Text>
+     <Image style={styles.imghome} source={img} />
+    </View>
+
+    // Os botões
       <Button title="Sobre" onPress={() => navigation.navigate("Sobre")} />
       <Button title="Contato" onPress={() => navigation.navigate("Contato")} />
       <Button title="Login" onPress={() => navigation.navigate("Login")} />
-      <Image style={styles.imghome} source={img} />
-    </View>
+    
+    
   );
 }
